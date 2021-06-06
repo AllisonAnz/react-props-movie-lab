768a1f5b2686a30b709552daa16eca5250b9f602
AllisonAnz <AllisonAnzalone@gmail.com>
2021-06-06 13:10:48 -0500

    Completed Assignment

--- src/MovieShowcase.js
+++ src/MovieShowcase.js
@@ -1,14 +1,25 @@
+//Houses all the raw data associated with the movies located in src/data.js
+
 import React, { Component } from 'react';
 import MovieCard from './card-components/MovieCard.js'
 import movieData from './data.js'
 
 export default class MovieShowcase extends Component {
 
+  //movieCard components receive their individual movie information as four props: title, IMDBRating, genres and psoter
   generateMovieCards = () => {
     // map over your movieData array and return an array of the correct JSX
+    return movieData.map((movie, index) => <MovieCard
+          key={index}
+          title={movie.title}
+          IMDBRating={movie.IMDBRating}
+          genres={movie.genres}
+          poster={movie.poster}
+          />)
   }
 
   render() {
+    //console.log(this.props)
     return (
       <div id="movie-showcase">
         {this.generateMovieCards()}
